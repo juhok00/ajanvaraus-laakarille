100413df4720b047479d2670729404530fc23041
juhok00 <163529510+juhok00@users.noreply.github.com>
2024-04-01 16:49:31 +0300

    Update users.py

--- users.py
+++ users.py
@@ -1,37 +1,44 @@
 from db import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.sql import text
 
 
 
 
 
-def login(username, password):
-    sql = "SELECT id, password FROM users WHERE username=:username"
+def login_user(username, password):
+    sql = text("SELECT id, password FROM users WHERE username=:username")
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
+        print("User not found")
         return False
     else:
-        if check_password_hash(user.password, password):
-            session["user_id"] = user.id
+        user_id, user_password_hash = user
+        if check_password_hash(user_password_hash, password):
+            session["user_id"] = user_id
+            print("User ID set in session:", session["user_id"])
             return True
         else:
+            print("Password check failed.")
             return False
         
 def logout():
     del session["user_id"]
     
 
-def register(username, password):
+def register_user(username, password):
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+        sql = text("INSERT INTO users (username,password) VALUES (:username,:password)")
         db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
-    except:
+        return True
+    except Exception as e:
+        print("Database insertion error:", e)
         return False
-    return login(username, password)
+
 
 def user_id():
     return session.get("user_id",0)
