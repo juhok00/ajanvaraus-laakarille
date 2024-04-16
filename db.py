9e143bea489a937d8e161624f2b83c065a9c14e1
juhok00 <163529510+juhok00@users.noreply.github.com>
2024-04-16 12:41:46 +0300

    Update db.py

--- db.py
+++ db.py
@@ -7,6 +7,3 @@ from os import getenv
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
-
-# miksi seuraava error: 
-# RuntimeError: Either 'SQLALCHEMY_DATABASE_URI' or 'SQLALCHEMY_BINDS' must be set.
