cf82201ddf8e45d0bf46cf66e99a51a6cf3b8c5a
juhok00 <163529510+juhok00@users.noreply.github.com>
2024-04-21 21:19:14 +0300

    Update appointments.py

--- appointments.py
+++ appointments.py
@@ -14,6 +14,16 @@ def get_available_times(date, doctor_id):
     
     
     result = db.session.execute(sql, {"date": date, "doctor_id": doctor_id})
-    times = [time[0] for time in result.fetchall()]
-    return times
+    return [time[0] for time in result.fetchall()]
 
+
+
+def save_appointment(doctor_id, date, time, details):
+    sql = text("""
+    INSERT INTO appointment (doctor_id, appointment_date, appointment_time, reserved, details)
+    VALUES (:doctor_id, :date, :time, True, :details)
+    RETURNING id
+    """)
+    result = db.session.execute(sql, {"doctor_id": doctor_id, "date": date, "time": time, "details": details})
+    db.session.commit()
+    return result.fetchone()[0]
