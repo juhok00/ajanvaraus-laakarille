0671db9442edca148ab029858f52becf828e4a91
juhok00 <163529510+juhok00@users.noreply.github.com>
2024-04-22 15:33:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.secret_key = getenv("SECRET_KEY")
+
+
+app.config['SECRET_KEY'] = getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+
+
 
 
 import routes
