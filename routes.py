aae3b3b1e14fd78de5add207fded5eaa4e627845
juhok00 <163529510+juhok00@users.noreply.github.com>
2024-05-05 20:33:08 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,43 +1,64 @@
 from app import app
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session#, flash, abort
 from users import login_user, register_user, logout
 from db import db
 from doctors import doctors_by_city
 from sqlalchemy.sql import text
 from confirmation import save_appointment, fetch_appointment_id
 from appointments import get_available_times
+#import secrets
 
+#def generate_csrf_token():
+#    if "csrf_token" not in session:
+#        session["csrf_token"] = secrets.token_hex(16)
+#    return session["csrf_token"]
 
 @app.route("/")
 def index():
+#    session["csrf_token"] = generate_csrf_token()
     return render_template("login.html")
 
 
 @app.route("/login",methods=["POST"])
 def login():
         
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+#    return session["csrf_token"]
+
+#    if session["csrf_token"] != request.form["csrf_token"]:
+#        abort(403)
+            
+            
+    username = request.form["username"]
+    password = request.form["password"]
         
-        if login_user(username, password):
-            return redirect("/location")
-        else:
-            return render_template("login.html", error= "Väärä käyttäjätunnus tai salasana")
+    if login_user(username, password):
+#        flash("Kirjautuminen onnistui", "success")
+        return render_template("location.html")
+        
     else:
-        return redirect("login.html")
+#        flash("Väärä käyttäjätunnus tai salasana", "error")
+        return render_template("login.html")
 
 
 
 @app.route("/register",methods=["POST"])
 def register():
+    
+#    return session["csrf_token"]
+    
+#    if session["csrf_token"] != request.form["csrf_token"]:
+#        abort(403)
+    
     new_username = request.form["new_username"]
     new_password = request.form["new_password"]
     
     if register_user(new_username, new_password):
+#        flash("Rekisteröidyit onnistuneesti", "success")
         return redirect("/")
+        
     else:
-        return render_template("login.html", error="Rekisteröinti epäonnistui")
+#        flash("Rekisteröinti epäonnistui", "error")
+        return render_template("register.html")
 
 @app.route("/logout")
 def logout():
@@ -48,9 +69,14 @@ def logout():
 
 @app.route("/location")
 def location():
+#    if session["csrf_token"] != request.form["csrf_token"]:
+#        abort(403)
     if "user_id" in session:
         sql = text("SELECT * FROM cities")
         cities = db.session.execute(sql).fetchall()
+        print(cities)   #debug
+        
+        
         return render_template("location.html", cities=cities)
     else:
         return redirect("/")
@@ -58,6 +84,10 @@ def location():
 
 @app.route("/doctors", methods=["POST"])
 def show_doctors():
+#    if session["csrf_token"] != request.form["csrf_token"]:
+#        abort(403)
+        
+        
     print("city id: ", request.form.get("city_id"))        # debug
     if "user_id" in session:
         city_id = request.form.get("city_id")
@@ -72,6 +102,9 @@ def show_doctors():
     
 @app.route("/pick_date", methods=["POST"])
 def pick_date():
+#    if session["csrf_token"] != request.form["csrf_token"]:
+#        abort(403)
+        
     doctor_id = request.form.get("doctor_id")
 
     if "user_id" in session:
@@ -85,6 +118,9 @@ def pick_date():
     
 @app.route("/appointments", methods=["POST"])
 def book_appointment():
+#    if session["csrf_token"] != request.form["csrf_token"]:
+#        abort(403)
+        
     selected_date = request.form.get("selected_date")
     doctor_id = request.form.get("doctor_id")
     if "user_id" in session and selected_date:
@@ -99,6 +135,10 @@ def book_appointment():
 
 @app.route("/confirmation", methods=["POST"])
 def confirm_appointment():
+#    if session["csrf_token"] != request.form["csrf_token"]:
+#        abort(403)
+        
+        
     doctor_id = request.form.get("doctor_id")
     details = request.form.get("details")
     date = request.form.get("appointment_date")
